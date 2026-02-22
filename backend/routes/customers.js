082d1881d545c17d1de5b3eb79020c66d637780b
KishoreKumar <kkishore5503@gmail.com>
2026-02-22 19:05:17 +0530

    Update customers.js

--- backend/routes/customers.js
+++ backend/routes/customers.js
@@ -22,12 +22,15 @@ console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
    const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
-  secure: false, // must be false for 587
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  connectionTimeout: 10000,
+  family: 4, // 🔥 FORCE IPv4 (VERY IMPORTANT)
+  tls: {
+    rejectUnauthorized: false
+  }
 });
 
     await transporter.sendMail({
