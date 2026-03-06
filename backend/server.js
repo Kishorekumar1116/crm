088a478b38e1de2011d6264eb27fc91b11205eaf
KishoreKumar <kkishore5503@gmail.com>
2026-03-06 11:59:46 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,14 +8,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// MongoDB
 mongoose.connect(process.env.MONGO_URI)
 .then(()=> console.log("MongoDB Connected"))
-.catch(err => console.log(err.message));
+.catch(err => console.log("Mongo Error:", err.message));
 
+// Routes
 app.use("/api/customers", require("./routes/customerRoutes"));
 app.use("/api/invoices", require("./routes/invoiceRoutes"));
 app.use("/api/quotations", require("./routes/quotationRoutes"));
 
-app.listen(process.env.PORT, ()=> {
-  console.log("Server Running");
+// Port
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=> {
+  console.log(`Server Running on ${PORT}`);
 });
