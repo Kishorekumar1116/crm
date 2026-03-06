3a189066a30eb2afda41ed12a5e5a00f1104df2c
KishoreKumar <kkishore5503@gmail.com>
2026-03-06 12:39:57 +0530

    Update sendJobMail.js

--- backend/utils/sendJobMail.js
+++ backend/utils/sendJobMail.js
@@ -2,48 +2,49 @@ const nodemailer = require("nodemailer");
 
 const sendJobMail = async (data) => {
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: "ipremiumindia@gmail.com",
-    pass: "mxwzukcfjefkucbv",
-  }
-});
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    }
+  });
 
-const mailOptions = {
-  from: "ipremiumindia@gmail.com",
-  to: data.email,
-  subject: "Service Job Created",
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: data.email,
+    subject: "Service Job Created",
 
-  html: `
-  <h2>Service Job Details</h2>
+    html: `
+    <h2>Service Job Details</h2>
 
-  <b>Job ID:</b> ${data.jobId} <br/>
-  <b>Job Date:</b> ${data.jobDate} <br/>
-  <b>Delivery Date:</b> ${data.deliveryDate} <br/>
+    <b>Job ID:</b> ${data.jobId} <br/>
+    <b>Job Date:</b> ${data.jobDate} <br/>
+    <b>Delivery Date:</b> ${data.deliveryDate} <br/>
 
-  <h3>Customer Details</h3>
-  <b>Name:</b> ${data.name} <br/>
-  <b>Phone:</b> ${data.phone} <br/>
-  <b>Email:</b> ${data.email} <br/>
-  <b>Company:</b> ${data.company} <br/>
+    <h3>Customer Details</h3>
+    <b>Name:</b> ${data.name} <br/>
+    <b>Phone:</b> ${data.phone} <br/>
+    <b>Email:</b> ${data.email} <br/>
+    <b>Company:</b> ${data.company} <br/>
 
-  <h3>Product Details</h3>
-  <b>Product:</b> ${data.productName} <br/>
-  <b>Brand:</b> ${data.brand} <br/>
-  <b>Model:</b> ${data.model} <br/>
-  <b>Serial No:</b> ${data.serialNo} <br/>
+    <h3>Product Details</h3>
+    <b>Product:</b> ${data.productName} <br/>
+    <b>Brand:</b> ${data.brand} <br/>
+    <b>Model:</b> ${data.model} <br/>
+    <b>Serial No:</b> ${data.serialNo} <br/>
 
-  <h3>Service Issue</h3>
-  <b>Issue:</b> ${data.issue} <br/>
-  <b>Additional:</b> ${data.additionalIssues} <br/>
+    <h3>Service Issue</h3>
+    <b>Issue:</b> ${data.issue} <br/>
+    <b>Additional:</b> ${data.additionalIssues || "-"} <br/>
 
-  <h3>Technician</h3>
-  ${data.technician}
-  `
-};
+    <h3>Technician</h3>
+    ${data.technician}
+    `
+  };
 
-await transporter.sendMail(mailOptions);
+  await transporter.sendMail(mailOptions);
+
 };
 
 module.exports = sendJobMail;
