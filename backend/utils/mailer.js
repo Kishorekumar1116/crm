171536b4ca3626a54823fb189d585d5f18d2f38a
KishoreKumar <kkishore5503@gmail.com>
2026-03-06 11:24:26 +0530

    Update mailer.js

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -3,8 +3,8 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "your-email@gmail.com", // Unga Gmail id
-    pass: "your-app-password",    // Unga Gmail App Password (Normal password illa)
+    user: "ipremiumindia@gmail.com",
+    pass: "mxwzukcfjefkucbv",   // Unga Gmail App Password (Normal password illa)
   },
 });
 
